18ab21321d746afe5777159291236981213def1c
IsabelaPivoto <pivotoisabela@email.com.br>
2026-03-10 13:35:10 -0300

    PrimeiroCommit

---
+++ aula2/Aula2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite 4 números inteiros: ");
+
+int A, B, C, D;
+
+Console.WriteLine("A:");
+
+Console.WriteLine("B:");
+
+Console.WriteLine("C:");
+
+Console.WriteLine("D:");
+
+int diferença=  (A * B) - (C * D);
+Console.WriteLine("Diferença =" + diferença);
