3256c9fbc1b3d3c27ccfdf0e53b9c7ec2243209e
IsabelaPivoto <pivotoisabela@email.com.br>
2026-06-09 14:01:59 -0300

    ex4

---
+++ aula7/ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] cidades = { "Vitória", "Belo Horizonte", "Rio de Janeiro", "São Paulo"};
+
+int[,] matriz ={ {0, 524, 521, 882}, {524, 0, 434, 586}, {521, 434, 0, 429}, {882, 586, 429, 0} };
+
+while (true )
+{
+    Console.WriteLine("Diga a sua cidade de origem:");
+
+    for( int i = 0; i < cidades.Length; i++)
+    {
+        Console.WriteLine($"{i} - {cidades[i]}");
+    }
+    int origem = int.Parse(Console.ReadLine() ?? "0");
+
+    Console.WriteLine("Diga onde vc quer ir :");
+    int destino = int.Parse(Console.ReadLine() ?? "0");
+
+    if (origem == destino)
+    {
+        Console.WriteLine("Origem e destino iguais!");
+        break;
+    }
+
+    Console.WriteLine($"Distância entre {cidades[origem]} e {cidades[destino]}:" + $"{matriz[origem, destino]} km");
+
+    Console.WriteLine();
+}
