b3326e8752c5e13c8bd94d893b1b153eeba349d0
IsabelaPivoto <pivotoisabela@email.com.br>
2026-07-07 14:01:02 -0300

    aula9

--- aula7/ex3/obj/Debug/net10.0/ex3.AssemblyInfo.cs
+++ aula7/ex3/obj/Debug/net10.0/ex3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024bd70d133ad382c413cb2914ae54946c00885e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e995b6ee2baa600104b3d606d96925b5f2acd09")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ aula9/ex4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+ static void Main()
+    {
+      Dictionary<string, string> jogos = new Dictionary<string, string>();
+
+      for(int i=1; i<=5; i++)
+        {
+            Console.WriteLine("Digite o nome do jogo:");
+            string nome = Console.ReadLine();
+
+             Console.WriteLine("Digite o gênero do jogo:");
+             string genero = Console.ReadLine();
+
+              jogos[nome] = genero;
+        }
+
+        Console.WriteLine("\n Digite o nome que deseja pesquisar:");
+        string pesquisa = Console.ReadLine();
+
+        if (jogos.ContainsKey(pesquisa))
+        {
+             Console.WriteLine("Gênero: " + jogos[pesquisa]);
+        }
+
+        else
+        {
+            Console.WriteLine("Jogo não cadastrado.");
+        }
+    }
+}
